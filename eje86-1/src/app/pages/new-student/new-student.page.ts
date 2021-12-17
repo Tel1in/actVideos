3d2a0b9750e5994eeb6ba58a8db63b2477a8403b
telloxw <panitelloor@ittepic.edu.mx>
2021-12-16 20:38:24 -0700

    PRAC 5 2

--- eje86-1/src/app/pages/new-student/new-student.page.ts
+++ eje86-1/src/app/pages/new-student/new-student.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { StudentService } from '../../services/student.service';
 
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-new-student',
   templateUrl: './new-student.page.html',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewStudentPage implements OnInit {
 
-  constructor() { }
+  myForm: FormGroup;
+  submitted = false;
+
+
+  constructor(private studentService: StudentService, public fb: FormBuilder) { }
+
 
   ngOnInit() {
+    this.myForm = this.fb.group(
+      {
+        name: ['',Validators.compose([Validators.required, Validators.minLength(6)])],
+        controlNumber: ['', Validators.compose([Validators.required])]
+      }
+    );
+  }
+
+  saveStudent(){
+    this.submitted= true;
+
+    if(this.myForm.valid){
+      this.studentService.newStudent({
+        name: this.myForm.get('name').value,
+        controlnumber: this.myForm.get('controlNumber').value,
+        active: false
+      });
+    }
   }
 
 }
